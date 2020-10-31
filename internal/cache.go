289f53640f2633dc001570bf34dd035b63b88a4e
Andreas Palm <andreaspalm98@gmail.com>
2020-10-31 14:25:40 +0100

    Added flag to get fullpath

--- internal/cache.go
+++ internal/cache.go
@@ -23,7 +23,7 @@ func NewCacheManager(cacheFolder string) *CacheManager {
 
 func (c *CacheManager) GetValue(ssid string) string {
 	var cache []CacheRecord
-	bytes, _ := ioutil.ReadFile(c.getPath() + "/cache.json")
+	bytes, _ := ioutil.ReadFile(c.getPath(true))
 	json.Unmarshal(bytes, &cache)
 
 	for index, record := range cache {
@@ -53,21 +53,12 @@ func (c *CacheManager) WriteCache(ssid string, value string) {
 	record := CacheRecord{ValidUntil: until, SSID: ssid, Value: value}
 	cache = append(cache, record)
 
-	file, err := os.OpenFile(c.getPath()+"/cache.json", os.O_RDWR|os.O_TRUNC, 0755)
-	if err != nil {
-		return // we do not care
-	}
-
-	bytes, err := json.Marshal(&cache)
-	_, err = file.Write(bytes)
-	if err != nil {
-		return // we do not care
-	}
+	c.write(cache)
 }
 
 func (c *CacheManager) getRecords() []CacheRecord {
 	var cache []CacheRecord
-	bytes, err := ioutil.ReadFile(c.getPath() + "/cache.json")
+	bytes, err := ioutil.ReadFile(c.getPath(true))
 	if err != nil {
 		c.createEmptyCacheFile()
 		bytes = []byte("[]")
@@ -79,7 +70,7 @@ func (c *CacheManager) getRecords() []CacheRecord {
 }
 
 func (c *CacheManager) write(cache []CacheRecord) {
-	file, err := os.OpenFile(c.getPath()+"/cache.json", os.O_RDWR|os.O_TRUNC, 0755)
+	file, err := os.OpenFile(c.getPath(true), os.O_RDWR|os.O_TRUNC, 0755)
 	if err != nil {
 		return // do we care?
 	}
@@ -96,12 +87,12 @@ func (c *CacheManager) write(cache []CacheRecord) {
 }
 
 func (c *CacheManager) createEmptyCacheFile() {
-	err := os.MkdirAll(c.getPath(), 0755)
+	err := os.MkdirAll(c.getPath(false), 0755)
 	if err != nil {
 		return // we do not care
 	}
 
-	file, err := os.Create(c.getPath() + "/cache.json")
+	file, err := os.Create(c.getPath(true))
 	if err != nil {
 		return // we do not care
 	}
@@ -109,6 +100,12 @@ func (c *CacheManager) createEmptyCacheFile() {
 	file.Close()
 }
 
-func (c *CacheManager) getPath() string {
-	return c.cacheFolder + "/weather-applet"
+func (c *CacheManager) getPath(file bool) string {
+	path := c.cacheFolder + "/weather-applet"
+	if file == true {
+		path += "/cache.json"
+		return path
+	}
+
+	return path
 }
